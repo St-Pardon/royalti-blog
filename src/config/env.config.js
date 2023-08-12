beb2fa5ab5df12b4a888bd94f96fd2e6b3cb25a7
St Pardon <okc4pardon@gmail.com>
2023-08-12 12:52:48 +0100

    :hammer: updated env variables

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -3,4 +3,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 5010
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-
+export const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key'
