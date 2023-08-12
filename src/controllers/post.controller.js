3b1ec79adfa03e9054f5bc15038cebf697e6dc6a
St Pardon <okc4pardon@gmail.com>
2023-08-12 19:17:06 +0100

    :hammer: added more controllers for posts
    including creation and getting controllers for posts

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,10 +1,76 @@
-import { PostModel } from '../models/posts.models';
+import { PostModel } from '../models/posts.models.js';
 
 class PostController {
-  static async createPost(req, res) {}
-  static async getAllPosts(req, res) {}
-  static async getPostById(req, res) {}
-  static async getPostByUser(req, res) {}
+  static async createPost(req, res) {
+    try {
+      const post = req.body;
+      const { _id: userid } = req.user;
+
+      if (!post) {
+        res.status(406).json({ error: 'Post body missing' });
+        return;
+      }
+
+      post.userid = userid;
+
+      await PostModel.create(post);
+
+      res.status(200).json({ message: 'Post created successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'An error occured' });
+    }
+  }
+  static async getAllPosts(req, res) {
+    // remember to rewrite and add pagination and filters
+    try {
+      const posts = await PostModel.find({ state: 'published' });
+
+      if (!posts || posts.length === 0) {
+        res.status(404).json({ error: 'No posts found' });
+        return;
+      }
+
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occured' });
+    }
+  }
+  static async getPostById(req, res) {
+    try {
+      const { postid } = req.params;
+
+      const post = await PostModel.findOneAndUpdate(
+        { _id: postid },
+        { $inc: { read_count: 1 } },
+        { new: true }
+      );
+
+      if (!post) {
+        res.status(404).json({ error: 'Post not found' });
+        return;
+      }
+
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occured' });
+    }
+  }
+  static async getPostByUser(req, res) {
+    try {
+      const { _id: userid } = req.user;
+
+      const posts = await PostModel.find({ userid });
+
+      if (!posts || posts.length === 0) {
+        res.status(404).json({ error: 'User have not created any post' });
+        return;
+      }
+
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occured' });
+    }
+  }
   static async publishPost(req, res) {
     try {
       const { postid } = req.params;
