bb3a2e479c39c03032cbb9a85d90a3c07461ae0f
St Pardon <okc4pardon@gmail.com>
2023-08-13 13:47:49 +0100

    :beetle: fixed server export error and included errorhandler

--- src/app.js
+++ src/app.js
@@ -3,18 +3,21 @@ import IndexRoute from './routes/index.route.js';
 import helmet from 'helmet';
 import './middlewares/auth.middleware.js';
 import { PORT } from './config/env.config.js';
+import { errHandler } from './middlewares/errorhandler.middleware.js';
 
 const app = express();
 
+app
+  .use(helmet())
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use(IndexRoute)
+  .use(errHandler)
+
 const server = () => {
-  app
-    .use(helmet())
-    .use(express.json())
-    .use(express.urlencoded({ extended: false }))
-    .use(IndexRoute)
-    .listen(PORT, () => {
-      console.log(`Server running on http://127.0.0.1:${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on http://127.0.0.1:${PORT}`);
+  });
 };
 
 export { server, app };
