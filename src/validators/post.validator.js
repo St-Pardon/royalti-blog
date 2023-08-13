3149333e159cefdda91cf2eb2b20419bd79851b1
St Pardon <okc4pardon@gmail.com>
2023-08-13 16:17:28 +0100

    :wrench: modified post validator

--- src/validators/post.validator.js
+++ src/validators/post.validator.js
@@ -2,11 +2,12 @@ import joi from 'joi';
 
 export const postValidator = joi.object({
   title: joi.string().min(5).max(255).required(),
-  description: joi.string().min(5).max(1000).optional(),
+  description: joi.string().min(5).max(1000).required(),
   author: joi.string().required(),
   body: joi.string().min(10).required(),
   userid: joi.string().required(),
-  tags: joi.array(),
+  tags: joi.array().required(),
   createdAt: joi.date().default(Date.now()),
   updatedAt: joi.date().default(Date.now()),
+  state: joi.string()
 });
