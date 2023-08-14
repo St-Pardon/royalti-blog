837547c4e2467cc458dba7a5696d1b8db77d3a62
St Pardon <okc4pardon@gmail.com>
2023-08-14 10:08:08 +0100

    :wrench: made provision for dynamic hostname

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import IndexRoute from './routes/index.route.js';
 import helmet from 'helmet';
 import './middlewares/auth.middleware.js';
-import { PORT } from './config/env.config.js';
+import { HOSTNAME, PORT } from './config/env.config.js';
 import { errHandler } from './middlewares/errorhandler.middleware.js';
 
 const app = express();
@@ -12,11 +12,15 @@ app
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
   .use(IndexRoute)
-  .use(errHandler)
+  .use(errHandler);
 
 const server = () => {
   app.listen(PORT, () => {
-    console.log(`Server running on http://127.0.0.1:${PORT}`);
+    console.log(
+      `Server running on ${
+        HOSTNAME.includes('127') ? HOSTNAME + ':' + PORT : HOSTNAME
+      }`
+    );
   });
 };
 
--- src/config/env.config.js
+++ src/config/env.config.js
@@ -4,3 +4,4 @@ dotenv.config();
 export const PORT = process.env.PORT || 5010
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 export const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key'
+export const HOSTNAME = process.env.HOSTNAME || 'http://127.0.0.1'
